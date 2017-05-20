f2b8289b5c17d61f02d191edea0bc812d0e55340
hkominos <hkominos@gmail.com>
2017-05-20 17:45:19 +0200

    added improved

--- src/improved/improved.c
+++ src/improved/improved.c
@@ -12,25 +12,24 @@
 int main(int argc, char *argv[])  {
 
 
-    int rank, nproc,array_size,i,chunk_size,global_pivot,current_rank;
-    int *Array,*local_array;
-    //*received_array;
+    int rank, nproc,array_size,i,chunk_size,global_pivot,current_rank,current_nproc;
+    int *Array,*local_array,*received_array;
     double start_time,total;   
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &nproc);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
     
-    //MPI_Status status;
-
-    MPI_Comm current_comm;    
+    MPI_Status status;
+    MPI_Comm current_comm,new_comm;    
     
     
     if (rank == 0){
 
         printf( "Give array size\n");
         scanf("%d",&array_size);
-        fflush(stdin);
+        //array_size=16;
+        fflush(stdin);       
 
         if(array_size%nproc!=0){
             printf("Size is not divisible by nproc. aborting");
@@ -40,7 +39,8 @@ int main(int argc, char *argv[])  {
         //largest in supported without overflow is 2,147,483,647
         
         start_time = MPI_Wtime();
-        Array = (int*)calloc(array_size , sizeof(int));
+        Array = (int*)malloc(array_size *sizeof(int));
+
 
         srand48((unsigned int)time(NULL));
         // Initialize the array with random values
@@ -51,65 +51,130 @@ int main(int argc, char *argv[])  {
         }
     }    
 
+
     //MPI_Barrier(MPI_COMM_WORLD);
     MPI_Bcast(&array_size, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     chunk_size = array_size/nproc;
-    local_array=(int*)calloc(chunk_size , sizeof(int));
+    local_array=(int*)malloc(chunk_size * sizeof(int));
     MPI_Scatter(Array, chunk_size, MPI_INT, local_array, chunk_size, MPI_INT, 0, MPI_COMM_WORLD);
-    
-    //const int MAX_NUMBERS = chunk_size*nproc;
 
+    
+    
+    const int MAX_NUMBERS = chunk_size*nproc;
+    received_array=(int*)malloc(MAX_NUMBERS * sizeof(int));
     int steps=(int)log2((int)nproc);
  
 
 
-    //steps=2;
-    //int dest=1;
-    
 
+     
     
     MPI_Comm_dup( MPI_COMM_WORLD, &current_comm );
     MPI_Comm_rank(current_comm, &current_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &current_nproc);
 
     int current_array_size=chunk_size;
     int* low_array=NULL;
     int* high_array=NULL;
     int low_array_size=0;
     int high_array_size=0;
+    int MPI_LOW_TAG=111;
+    int MPI_HIGH_TAG=222;
+    int LOW_SET=1;
+    int HIGH_SET=10;
+    int received_array_size=0;
       
     for(i=1;i<=steps;i++){
         if(current_rank==0){global_pivot=local_array[2];}
         MPI_Bcast(&global_pivot, 1, MPI_INT, 0, current_comm);
-
-        //printf("I received %d and %d %d \n",low, high, Array[0]);
-        if(current_rank==0){sleep(5);
-                        printf("\n\n\n");
-                        }
+        
         partition_with_pivot(0,current_array_size,local_array,global_pivot,&low_array,&high_array,&low_array_size,&high_array_size,current_rank);
+        if (local_array!=NULL)free(local_array);
 
-        printf("Sizes received are high %d and low %d\n",high_array_size,low_array_size );
+        float middle=current_nproc/2;
 
-         printf("low array is \n");
-        for(i=0;i<=low_array_size-1;i++){
+        if (current_rank<middle){
+            //printf("SEND at %lf\n",MPI_Wtime());
+            MPI_Send(high_array, high_array_size, MPI_INT, current_rank+middle, MPI_HIGH_TAG, current_comm);       
+            MPI_Recv(received_array, MAX_NUMBERS, MPI_INT, current_rank+middle, MPI_LOW_TAG, current_comm, &status); 
+            MPI_Get_count(&status, MPI_INT, &received_array_size);
+            //printf("RCV at %lf\n with size %d",MPI_Wtime(),received_array_size);       
+            local_array=merge_arrays(low_array,low_array_size,received_array,received_array_size);
+            current_array_size=received_array_size+low_array_size;
+            printf("Merge at %lf with array size %d \n",MPI_Wtime(),current_array_size);
+            myqsort(0, current_array_size-1,local_array);
+
+            for(i=0;i<=current_array_size-1;i++){            
+            printf("%d\n",local_array[i]);
+            }
+
+
             
+            printf("Sorted at %lf\n",MPI_Wtime());
+
+
+
+
+            
+                
+        }else
+        {       
+            //printf("RCV at %lf\n with size %d",MPI_Wtime(),received_array_size);     
+            MPI_Recv(received_array, MAX_NUMBERS, MPI_INT, current_rank-middle, MPI_HIGH_TAG, current_comm, &status);           
+            MPI_Get_count(&status, MPI_INT, &received_array_size);
+            MPI_Send(low_array, low_array_size, MPI_INT, current_rank-middle, MPI_LOW_TAG, current_comm); 
+            //printf("SEND at %lf\n",MPI_Wtime());           
+            local_array=merge_arrays(high_array,high_array_size,received_array,received_array_size);
+            current_array_size=received_array_size+high_array_size;
+            printf("Merge at %lf with array size %d \n",MPI_Wtime(),current_array_size);
+            //myqsort(0, current_array_size-1,local_array);
+            printf("un Sorted at %lf\n",MPI_Wtime());
+
+                
+        }
+
+
+        int color;
+        if (current_rank<middle){color=LOW_SET;}
+            else{color=HIGH_SET;}
+
+        MPI_Comm_split(current_comm, color, current_rank, &new_comm);
+
+        MPI_Comm_dup( new_comm, &current_comm );
+        MPI_Comm_rank(current_comm, &current_rank);
+        MPI_Comm_size(MPI_COMM_WORLD, &current_nproc);
+
+        
+         /*
+        for(i=0;i<=current_array_size-1;i++){            
             printf("%d\n",low_array[i]);
             }
 
 
+
+
+            
+
+        printf("Sizes received are high %d and low %d\n",high_array_size,low_array_size );
+         printf("low array is \n");
+        for(i=0;i<=low_array_size-1;i++){            
+            printf("%d\n",low_array[i]);
+            }
             printf("hign array is \n");
         for(i=0;i<=high_array_size-1;i++){
             printf("%d\n",high_array[i]);
-            
-            }
-
-       
+                        }      
+        */
         
 
 
-          
-        
 
     }
+
+    //printf("time is %d\n",current_array_size );
+    //myqsort(0, current_array_size,local_array);
+
+    //printf("Processor with rank %d reports %d\n",rank,isSorted(local_array,current_array_size-1) );
        
     
     MPI_Barrier(MPI_COMM_WORLD);
@@ -118,13 +183,13 @@ int main(int argc, char *argv[])  {
     if (rank==0){
         total = MPI_Wtime() - start_time;
         printf("\nTotal time taken: %f seconds",total);
-        int Result=isSorted(local_array,array_size);
-        if (Result==YES)printf("Array Sorted! \n");
-        free(local_array);
+        //int Result=isSorted(local_array,array_size);
+        //if (Result==YES)printf("Array Sorted! \n");
+        //free(local_array);
         UserChoice();
     }
     else{
-        if (local_array!=NULL)free(local_array);
+        //if (local_array!=NULL)free(local_array);
     }
 
     MPI_Finalize();
@@ -139,9 +204,6 @@ int main(int argc, char *argv[])  {
 void partition_with_pivot(int low, int high, int* Array,int pivot,int** low_array, int** high_array, int* low_array_size, int* high_array_size,int current_rank){
     int i,temp_value;   
 
-    for(i=0;i<=high-1;i++){
-            printf("%d\n",Array[i]);
-            } 
 
     if(current_rank==0){
         for(i=low;i<=high-1;i++){
@@ -202,9 +264,6 @@ void partition_with_pivot(int low, int high, int* Array,int pivot,int** low_arra
     int lowsize=*low_array_size;
     int highsize=*high_array_size;
     
-    
-
-
 
     if(lowsize!=0 && high_array_size!=0){
     *low_array=(int *)malloc(sizeof(int)*lowsize);
@@ -233,26 +292,6 @@ void partition_with_pivot(int low, int high, int* Array,int pivot,int** low_arra
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 int _getelement(){
     return 2;
 }
@@ -261,54 +300,23 @@ int _getelement(){
 
 
 
-int* merge_arrays(int *array1, int *array2, int array_size){
+int* merge_arrays(int *array1, int array1_size,int *array2, int array2_size){
 
-    int* new_array=(int*)calloc(array_size*2 , sizeof(int));
+    int* new_array=(int*)malloc((array1_size+array2_size)* sizeof(int));
+    int i,temp;
 
-   
-    
-    int start_of_array1=array1[0];
-    int start_of_array2=array2[0];
-    int i;
-    int index_1=0;
-    int index_2=0;
-   
-
-    if(start_of_array2<start_of_array1){
-        int* temp=array2;
-        array2=array1;
-        array1=temp;
+    for(i=0;i<array1_size;i++){        
+        new_array[i]=array1[i];
+        //temp=i;
     }
-   
-    
-   for(i=0;i<array_size*2;i++){
+    temp=array1_size;
+    for(i=0;i<array2_size;i++){        
+        new_array[temp+i]=array2[i];
+    }
 
-        if(index_1>=array_size)
-        {
-            new_array[i]=array2[index_2];
-            index_2++;
-        }
-        else if (index_2>=array_size)
-        {
-            new_array[i]=array1[index_1];
-            index_1++;
-        }
+    free(array1);
+    free(array2);  
 
-        else if(array1[index_1]<=array2[index_2] && index_1<array_size && index_2<array_size)
-        {
-            new_array[i]=array1[index_1];
-            index_1++;
-        }
-
-        else{
-            new_array[i]=array2[index_2];
-            index_2++;
-        }
-   
-
-
-   }        
-   
     return new_array;
 
 }
@@ -370,5 +378,3 @@ int partition(int low, int high, int* Array) {
     swap(index,high,Array);
     return index;
 }
-
-
--- src/serial/main.c
+++ src/serial/main.c
@@ -7,11 +7,6 @@
 
 int main(int argc, char *argv[])  {
 
-
-    int rank, nproc;   
-    MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &nproc);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     
     Serial_Qsort();   
     UserChoice();
